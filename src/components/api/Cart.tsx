a9de19211d6450d38d702370e043c29b43afb318
youssef <ya53114808@gmail.com>
2026-06-10 23:54:17 +0300

    f

--- src/components/api/Cart.tsx
+++ src/components/api/Cart.tsx
@@ -6,25 +6,27 @@ import {
 	DialogTitle,
 	DialogTrigger,
 } from "@/components/ui/dialog";
+import { Button } from "../ui/button";
 import Cards from "./Cards/Cards";
 import { useProducts } from "./Context/ShoppingCartContext";
 import EmptyCart from "./EmptyCart";
-import { Button } from "../ui/button";
 
 function Cart() {
 	const { cart } = useProducts();
 
 	return (
 		<Dialog>
-			<DialogTrigger><Button>Open Cart</Button></DialogTrigger>
+			<DialogTrigger>
+				OPEN CART
+			</DialogTrigger>
 			<DialogContent>
 				<DialogHeader>
 					<DialogTitle>Cart</DialogTitle>
 					<DialogDescription>There is a popup</DialogDescription>
-				</DialogHeader>
-					<div className="overflow-y-scroll max-h-[60vh]">
+					<DialogContent className="overflow-y-scroll max-h-[60vh]">
 						{cart.length > 0 ? <Cards cart={true} /> : <EmptyCart />}
-					</div>
+					</DialogContent>
+				</DialogHeader>
 			</DialogContent>
 		</Dialog>
 	);
