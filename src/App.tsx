23d9702de3252e9f19322110e67dee595aa93fb7
youssef <ya53114808@gmail.com>
2026-06-11 01:02:09 +0300

    final course

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
 import Api from "./components/api/Api";
 import ShoppingCartProvider from "./components/api/Context/ShoppingCartContext";
 import Counter from "./components/Counter/Counter";
+import ThreeCounters from "./components/Counters/ThreeCounters";
 import Hello from "./components/Hello";
+import Memo from "./components/Memo";
 import Names from "./components/Names";
 import Auth from "./components/SignIn/Auth";
-import ThreeCounters from "./components/Counters/ThreeCounters";
 import Timer from "./components/Timer";
-import Memo from "./components/Memo";
 
 export function App() {
 	return (
 		<>
 			<Hello />
-		<main>
-			<Counter />
-			<Names />
-			<Auth />
-			<ShoppingCartProvider>
-				<Api />
-			</ShoppingCartProvider>
-			<Timer />
-			<ThreeCounters />
-			<Memo />
-		</main>
+			<main>
+				<Counter />
+				<Names />
+				<Auth />
+				<ShoppingCartProvider>
+					<Api />
+				</ShoppingCartProvider>
+				<Timer />
+				<ThreeCounters />
+				<Memo />
+			</main>
 		</>
 	);
 }
